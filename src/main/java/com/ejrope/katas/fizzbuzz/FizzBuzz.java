7959dd8ac3f22c514befccfc12a25d2e8d110637
Emilio J. Rodríguez <ejrope@gmail.com>
2015-07-02 20:59:17 +0200

    Initial commit with main class and main test

---
+++ src/main/java/com/ejrope/katas/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.ejrope.katas.fizzbuzz;
+
+/**
+ * Created by desyfm on 2/07/15.
+ */
+public class FizzBuzz {
+}
---
+++ src/test/java/com/ejrope/katas/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package com.ejrope.katas.fizzbuzz;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @Test
+    public void process() {
+        fizzBuzz = new FizzBuzz();
+    }
+}
